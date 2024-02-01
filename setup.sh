d133b6b4c3f7e1d825c619facb7da00960f6e847
mcfrojd <mcfrojd@gmail.com>
2024-02-01 13:46:19 +0100

    code cleanup

--- setup.sh
+++ setup.sh
@@ -103,9 +103,7 @@ echo ""
 read -s -n 1
 clear
 
-#
 # copy the nessesary file to /etc/init.d.
-#
 echo "copy the nessesary file to /etc/init.d."
 echo ""
 cp /data/persist_custom_settings_venusos-master/files/tailscaled /etc/init.d/
@@ -117,9 +115,7 @@ echo ""
 read -s -n 1
 clear
 
-#
 # make init script executable
-#
 echo "make init script executable"
 echo ""
 chmod +x /etc/init.d/tailscaled
@@ -131,9 +127,7 @@ echo ""
 read -s -n 1
 clear
 
-#
 # test the init script
-#
 echo "test the init script"
 echo ""
 /etc/init.d/tailscaled start
@@ -147,9 +141,7 @@ echo ""
 read -s -n 1
 clear
 
-#
 # configure tailscale init script to start automatically on boot
-#
 echo "configure tailscale init script to start automatically on boot"
 echo ""
 update-rc.d tailscaled defaults
@@ -160,9 +152,7 @@ echo ""
 read -s -n 1
 clear
 
-#
 # enable ip-forward
-#
 echo "enable ip-forward"
 echo ""
 echo 'net.ipv4.ip_forward = 1' | sudo tee -a /etc/sysctl.conf
@@ -175,16 +165,11 @@ echo ""
 read -s -n 1
 clear
 
-#
 # connect to your tailscale account
 # Theese settings are custom for my setup, change the ip address to match your system!
-#
 echo "connect to your tailscale account"
 echo ""
 echo 'run "tailscale up -ssh --advertise-routes=192.168.77.0/24"'
 echo ""
-#
-# if there was no errors, tailscale should be installed and ready!
-#
 echo "if there was no errors, tailscale should be installed and ready!"
 echo ""
